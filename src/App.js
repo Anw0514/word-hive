197b195dc710dc8bcbde7b5dcd2811c287bd4c92
anw0514 <anw0514@gmail.com>
2023-08-14 15:25:17 -0700

    installed dependencies and implemented color theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, styled } from "styled-components";
+
+const theme = {
+  dark: "#140001",
+  light: "#CDD3E0",
+  primary: "#F3C33F",
+  secondary: "#564110",
+  success: "#7CB441",
+  warning: "#FF7247",
+  failure: "#FF4747",
+  info: "#009FB7"
+}
+
+const StyledApp = styled.div`
+  background-color: ${props => props.theme.dark};
+  color: ${props => props.theme.primary};
+  min-height: 100vh;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        Hello World
+      </StyledApp>
+    </ThemeProvider>
   );
 }
 
