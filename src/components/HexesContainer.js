0e514fac176d45c8a0ae190f4e6752cf3b1956d3
anw0514 <anw0514@gmail.com>
2023-08-15 17:03:20 -0700

    deleted unnecessary css variables

--- src/components/HexesContainer.js
+++ src/components/HexesContainer.js
@@ -4,10 +4,6 @@ import { generateGridArray } from '../utils/gridHelpers'
 import Hex from './Hex'
 
 const StyledHexesContainer = styled.div`
-    --r: 0.24935;
-	--r-1: calc(1 / calc(1 - var(--r)));
-	--g: 2px;
-	--w: calc(var(--r-1) * var(--wrp) / 5 - 2 * var(--r-1) * var(--g));
 	margin: 0 -1px;
 	display: flex;
 	flex-wrap: wrap;
