e0591f9f612e86547072622e62cafb636b9201f5
anw0514 <anw0514@gmail.com>
2023-09-06 23:35:48 -0700

    styled score

--- src/components/ScoreContainer.js
+++ src/components/ScoreContainer.js
@@ -7,12 +7,21 @@ const StyledScoreContainer = styled.div`
     color: ${props => props.theme.primary};
     opacity: .7;
     width: ${props => props.$horizontal ? '80%' : '90%'};
-    margin-bottom: 10px;
+    margin-bottom: 5px;
+    font-size: 1.3em;
+    line-height: .7em;
+
+    div {
+        font-size: .4em;
+    }
 `
 export default function ScoreContainer() {
-    const score = useSelector(state => state.app.score)
+    const score = useSelector(state => state.word.score)
     const isHorizontal = useSelector(state => state.app.isHorizontal)
     return (
-        <StyledScoreContainer $horizontal={isHorizontal}>Current Score: {score}</StyledScoreContainer>
+        <StyledScoreContainer $horizontal={isHorizontal}>
+            {score}
+            <div>Current Score</div>
+        </StyledScoreContainer>
     )
 }
